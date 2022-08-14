9b2a7a87149e945705f7f540d754a54e6d48e714
raju <rajubheemaraju96@gmail.com>
2022-08-14 17:27:07 +0530

    my task completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,6 @@
 this is java reg page
-this my manager work completed
+1st day java lines of code
+2nd day java lines of code
+3rd day java lines of code
+4th day java lines of code
+5th day java lines of code completed task
